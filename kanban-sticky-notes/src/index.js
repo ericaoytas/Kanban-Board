93f9e3ef64e2fd9c34d5ab12b54823dc0f8a409a
Erica Oytas <ericaoytas@gmail.com>
2020-10-01 16:54:27 -0700

    Relocate files

--- kanban-sticky-notes/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './fontawesome'
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
