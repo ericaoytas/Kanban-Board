d304421c4058c37f935e41aa23a7c7b5580abeb3
Erica Oytas <ericaoytas@gmail.com>
2020-10-06 17:29:51 -0700

    Update TestMain.java

--- Kanban Sticky Notes/src/main/java/eo/kanbanstickynotes/test/TestMain.java
+++ Kanban Sticky Notes/src/main/java/eo/kanbanstickynotes/test/TestMain.java
@@ -3,6 +3,13 @@ package eo.kanbanstickynotes.test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import eo.kanbanstickynotes.entity.*;
+
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -19,6 +26,35 @@ public class TestMain {
 			e.printStackTrace();
 		}
 
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Board.class)
+								.addAnnotatedClass(Category.class)
+								.addAnnotatedClass(Color.class)
+								.addAnnotatedClass(Note.class)
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// Create Board object
+			Board board = new Board("Project X");
+			
+			// Start transaction
+			session.beginTransaction();
+			
+			// Save the Board
+			session.save(board);
+			
+			// Commit the transaction
+			session.getTransaction().commit();
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+		
 	}
 
 }
