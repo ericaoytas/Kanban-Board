a02dc58b6f7a1a8f2e04e053c0b6653ec1287f6a
Erica Oytas <ericaoytas@gmail.com>
2020-11-05 18:45:52 -0800

    Create Note DAO

---
+++ Kanban Sticky Notes/src/main/java/eo/kanbanstickynotes/dao/ColorDAO.java
@@ -0,0 +1,11 @@
+package eo.kanbanstickynotes.dao;
+
+import eo.kanbanstickynotes.entity.Color;
+
+public interface ColorDAO extends DAO<Color>{
+	//Create
+	//Read
+	public Color getRandomColor();
+	//Update
+	//Delete
+}
