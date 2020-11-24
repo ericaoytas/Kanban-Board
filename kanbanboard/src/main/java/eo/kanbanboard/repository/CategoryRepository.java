15a5fee7ea0a39f624ccfb65b9e603fe45055469
Erica Oytas <ericaoytas@gmail.com>
2020-11-23 16:29:58 -0800

    Create CategoryRepository.java

---
+++ kanbanboard/src/main/java/eo/kanbanboard/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package eo.kanbanboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import eo.kanbanboard.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
