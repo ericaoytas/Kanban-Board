740450f7f2739cfe0be38e601044b8fe0dc34b9c
Erica Oytas <ericaoytas@gmail.com>
2020-12-01 18:49:02 -0800

    Update App.jsx
    Update imports

--- Kanban React Client/src/containers/App.jsx
+++ Kanban React Client/src/containers/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Header from '../components/templates/Header';
-import Footer from '../components/templates/Footer';
+import Header from '../templates/Header';
 import KanbanBoard from '../components/KanbanBoard/KanbanBoard';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import noteData from '../noteData.jsx';
 
 
 function App() {
